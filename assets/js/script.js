fdbb2262b42e858fb91c1d5ebf2e2c90150035db
Abhishek Raj <abhishek.raj@nagarro.com>
2018-04-21 22:42:59 +0530

    updated code: A Raj

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,9 @@ document.getElementById('mobcross').addEventListener("click", function () {
     document.getElementById('mobmenulist').style.display = 'none';
     document.getElementById('mobmenu').style.display = 'block';
 })
+
+
+function underDevelopment(){
+    alert("This page is under development. Try after some time.");
+    return false;
+}
